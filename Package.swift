bcb4c78906fa9b43dd63bafc4f45c911c427adcd
Anthony Drendel <me@anthonydrendel.com>
2021-04-10 00:40:53 +0200

    Expose IdentifierDynamic dynamic library

--- Package.swift
+++ Package.swift
@@ -7,14 +7,18 @@ let package = Package(
         .library(
             name: "Identifier",
             targets: ["Identifier"]),
+        .library(
+            name: "IdentifierDynamic",
+            type: .dynamic,
+            targets: ["Identifier"]),
     ],
     dependencies: [
-        .package(name: "Bytes", url: "https://github.com/shareup/bytes-apple.git", from: "1.0.0")
+        .package(name: "Bytes", url: "https://github.com/shareup/bytes-apple.git", from: "2.1.0")
     ],
     targets: [
         .target(
             name: "Identifier",
-            dependencies: ["Bytes"]),
+            dependencies: [.product(name: "BytesDynamic", package: "Bytes")]),
         .testTarget(
             name: "IdentifierTests",
             dependencies: ["Identifier"]),
